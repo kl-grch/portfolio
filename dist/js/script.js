528302b3b44013d525f2ca80ec1eee5d24f78308
Nikolay Gerasimov <kg@MacBook-Air-Nikolay.local>
2022-09-26 11:54:09 +0300

    full

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,3 +11,10 @@ humburger.addEventListener('click', function() {
 close.addEventListener('click', function() {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
